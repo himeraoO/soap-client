09e3780617c602fcdfb6d918ac675bde7fd53234
himeraoO <himer@bk.ru>
2022-07-06 22:27:29 +0300

    Update

--- src/main/java/com/example/soapclient/SoapClientApplication.java
+++ src/main/java/com/example/soapclient/SoapClientApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.util.stream.Collectors;
-
 @SpringBootApplication
 public class SoapClientApplication {
 
@@ -25,13 +23,51 @@ public class SoapClientApplication {
             String name = "Spring";
             String password = "S2p3ring";
             AddUserDetailsResponse addUserDetailsResponse = usersClient.addUser(login, name, password, 1L, 2L );
+
             UserDetails userDetails = addUserDetailsResponse.getUserDetails();
             if (userDetails != null) {
                 System.out.println(userDetails.getLogin() + ", "+ userDetails.getUsername());
             }
             ServiceStatus serviceStatus = addUserDetailsResponse.getServiceStatus();
-            System.out.println("StatusCode: " + serviceStatus.getStatusCode() +
+            System.out.println("StatusCode: " + serviceStatus.getStatus() +
                     ", Message: " + serviceStatus.getMessage());
+            if (!serviceStatus.getError().isEmpty()){
+                serviceStatus.getError().forEach(errorSOAP -> System.out.println(errorSOAP.getMessage()));
+            }
+            /////////////////////////////////////////////////////////
+            System.out.println("--- Add User ---");
+            String login2 = "";
+            String name2 = "";
+            String password2 = "";
+            AddUserDetailsResponse addUserDetailsResponse2 = usersClient.addUser(login2, name2, password2, 1L, 2L );
+
+            UserDetails userDetails2 = addUserDetailsResponse2.getUserDetails();
+            if (userDetails2 != null) {
+                System.out.println(userDetails2.getLogin() + ", "+ userDetails2.getUsername());
+            }
+            ServiceStatus serviceStatus2 = addUserDetailsResponse2.getServiceStatus();
+            System.out.println("StatusCode: " + serviceStatus2.getStatus() +
+                    ", Message: " + serviceStatus2.getMessage());
+            if (!serviceStatus2.getError().isEmpty()){
+                serviceStatus2.getError().forEach(errorSOAP -> System.out.println(errorSOAP.getMessage()));
+            }
+            /////////////////////////////////////////////////////////
+            System.out.println("--- Add User ---");
+            String login3 = "";
+            String name3 = "";
+            String password3 = "qweqweqweqwe";
+            AddUserDetailsResponse addUserDetailsResponse3 = usersClient.addUser(login3, name3, password3, 1L, 2L );
+
+            UserDetails userDetails3 = addUserDetailsResponse3.getUserDetails();
+            if (userDetails3 != null) {
+                System.out.println(userDetails3.getLogin() + ", "+ userDetails3.getUsername());
+            }
+            ServiceStatus serviceStatus3 = addUserDetailsResponse3.getServiceStatus();
+            System.out.println("StatusCode: " + serviceStatus3.getStatus() +
+                    ", Message: " + serviceStatus3.getMessage());
+            if (!serviceStatus3.getError().isEmpty()){
+                serviceStatus3.getError().forEach(errorSOAP -> System.out.println(errorSOAP.getMessage()));
+            }
             /////////////////////////////////////////////////////////
             System.out.println("--- Get all Users ---");
             GetUsersResponse getUsersResponse = usersClient.getAllUsers();
@@ -51,9 +87,12 @@ public class SoapClientApplication {
             userAllDetailsUpd.setUsername("NewSpring");
             userAllDetailsUpd.setPassword("1Q2W23asdasd");
             UpdateUserDetailsResponse updateArticleResponse = usersClient.updateUser(userAllDetailsUpd, 2L);
-            ServiceStatus serviceStatus2 = updateArticleResponse.getServiceStatus();
-            System.out.println("StatusCode: " + serviceStatus2.getStatusCode() +
-                    ", Message: " + serviceStatus2.getMessage());
+            ServiceStatus serviceStatus4 = updateArticleResponse.getServiceStatus();
+            System.out.println("StatusCode: " + serviceStatus4.getStatus() +
+                    ", Message: " + serviceStatus4.getMessage());
+            if (!serviceStatus4.getError().isEmpty()){
+                serviceStatus4.getError().forEach(errorSOAP -> System.out.println(errorSOAP.getMessage()));
+            }
             //////////////////////////////////////////////////////////
             System.out.println("--- Get all Users ---");
             getUsersResponse = usersClient.getAllUsers();
@@ -61,11 +100,11 @@ public class SoapClientApplication {
                     .forEach(e -> System.out.println(e.getLogin() + ", "+ e.getUsername()));
             /////////////////////////////////////////////////////////
             System.out.println("--- Delete User ---");
-            String login2 = "wer";
-            DeleteUserDetailsResponse deleteUserDetailsResponse = usersClient.deleteUser(login2);
-            ServiceStatus serviceStatus3 = deleteUserDetailsResponse.getServiceStatus();
-            System.out.println("StatusCode: " + serviceStatus3.getStatusCode() +
-                    ", Message: " + serviceStatus3.getMessage());
+            String login4 = "wer";
+            DeleteUserDetailsResponse deleteUserDetailsResponse = usersClient.deleteUser(login4);
+            ServiceStatus serviceStatus5 = deleteUserDetailsResponse.getServiceStatus();
+            System.out.println("StatusCode: " + serviceStatus5.getStatus() +
+                    ", Message: " + serviceStatus5.getMessage());
             //////////////////////////////////////////////////////////
             System.out.println("--- Get all Users ---");
             getUsersResponse = usersClient.getAllUsers();
